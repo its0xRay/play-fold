4d0550f8b15618be7e6170e775c2d238f007fd43
its0xRay <99510382+its0xRay@users.noreply.github.com>
2023-11-28 12:06:58 +0530

    Input Validation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './App.css'; // Add your styles
 
+const minLength = 2;
+
 function App() {
   const [card1, setCard1] = useState('');
   const [card2, setCard2] = useState('');
@@ -9,11 +11,18 @@ function App() {
   const formatCard = (card) => {
     const cardRank = card.slice(0, -1);
     const cardSuit = card.slice(-1);
-    const suits = { S: '♠', D: '♦', H: '♥', C: '♣' };
+    const suits = { S: '♠️', D: '♦️', H: '❤️', C: '♣️' };
     return cardRank + suits[cardSuit];
   };
 
   const calculateProbability = () => {
+
+    // Check if card inputs meet the minLength requirement
+  if (card1.length < minLength || card2.length < minLength) {
+    setResult(`Please enter valid card with ${minLength} characters.`);
+    return;
+  }
+
     const premiumCards = ['A', 'K'];
     const highCards = ['Q', 'J', '10'];
     const lowCards = ['2', '3', '4', '5', '6', '7', '8', '9'];
@@ -22,8 +31,7 @@ function App() {
     const card2Rank = card2.substring(0, card2.length - 1);
 
     if (
-      premiumCards.includes(card1Rank) &&
-      premiumCards.includes(card2Rank)
+      premiumCards.includes(card1Rank) && premiumCards.includes(card2Rank)
     ) {
       setResult('Probability of winning is very high. May consider playing.');
     } else if (
@@ -43,15 +51,15 @@ function App() {
   return (
     <div className="container">
       <h1>Poker Helper</h1>
-      <p>
-        Enter the card name/number(Ace: "A", King: "K", Queen: "Q", Jack: "J",
-        9, 8, 7, 6, 5, 4, 3, 2, 1)
+      <h3>How to use?</h3>
+        <p>
+            Enter the card name/number followed by the first letter of the suit 
         <br />
-        Followed by the first letter of the suit (Heart: "H", Diamond: "D",
-        Club: "C", Spade: "S")
+        Card name/number (Ace: "A", King: "K", Queen: "Q", Jack: "J",
+        9, 8, 7, 6, 5, 4, 3, 2, 1) followed by suit (Heart: "H", Diamond: "D", Club: "C", Spade: "S")
       </p>
       <h3>Starting Hands</h3>
-      <label htmlFor="card1">Card 1:</label>
+      <label htmlFor="card1">Card 1: </label>
       <input
         type="text"
         id="card1"
@@ -61,7 +69,7 @@ function App() {
         onChange={(e) => setCard1(e.target.value.toUpperCase().trim())}
       />
       <br />
-      <label htmlFor="card2">Card 2:</label>
+      <label htmlFor="card2">Card 2: </label>
       <input
         type="text"
         id="card2"
@@ -73,7 +81,7 @@ function App() {
       <br />
       <button onClick={calculateProbability}>Calculate</button>
       <div className="container1">
-        <h2>Your Hand:</h2>
+        <h3>Your Hand:</h3>
         <div id="user-card1" className="card">
           {formatCard(card1)}
         </div>
